69a0505263366f7bcd841677917fb8785f95ffe5
Barbara Pobatschnig <curby@barbarahnigsmbp.home>
2014-10-02 22:20:07 +0200

    Initial commit

---
+++ wear/src/main/java/com/example/curby/helloworld/MainAcitivity.java
@@ -0,0 +1,24 @@
+package com.example.curby.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainAcitivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_acitivity);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
